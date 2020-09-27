f5bc340e7d25f4f8a29093d439a268ecd61ef487
lusamdebonfils <samuelluswata@gmail.com>
2020-09-27 16:17:52 -0400

    Added spring boot producer configs for kafka and kafka streams

---
+++ Kafka-Tweets-producer/src/main/java/cs/edu/mum/cs523/Kafka/Tweets/producer/KafkaTweetsProducerApplication.java
@@ -0,0 +1,13 @@
+package cs.edu.mum.cs523.Kafka.Tweets.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTweetsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTweetsProducerApplication.class, args);
+	}
+
+}
---
+++ Kafka-Tweets-producer/src/test/java/cs/edu/mum/cs523/Kafka/Tweets/producer/KafkaTweetsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.edu.mum.cs523.Kafka.Tweets.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTweetsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
